1962ed62bd73e6071f7fda89b86f78acdeacf0b2
tsetse0725 <tse9406@gmail.com>
2025-03-19 12:29:28 +0800

    array method update hiisen.

---
+++ arrayMethods.js
@@ -0,0 +1,24 @@
+let numbers = [10, 20, 30, 40, 50];
+
+numbers.push(60);
+
+console.log(numbers);
+
+numbers.unshift(5);
+
+console.log(numbers);
+
+numbers.shift();
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+numbers.reverse();
+console.log(numbers);
+
+numbers.splice(1, 1, 100);
+console.log(numbers)
+
+let number = [3, 6 ,9 ,12, 15];
+
